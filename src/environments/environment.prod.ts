65aad82f0fe3c89a7292b07106143d81f7180e22
Randall Valenciano <ravf.226@gmail.com>
2018-03-25 23:46:44 -0600

    sin-i133: adds proper url to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ import env from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'https://api.chucknorris.io',
+  serverUrl: 'http://stage.sinamecc-minae.com:8000/api',
   defaultLanguage: 'en-US',
   supportedLanguages: [
     'en-US',
