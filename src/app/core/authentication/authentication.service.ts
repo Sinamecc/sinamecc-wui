05b71d500d8e1b551ff3d29195854c0d7c43ada2
Randall Valenciano <ravf.226@gmail.com>
2018-03-26 22:16:38 -0600

    sin-i133: delete options

--- src/app/core/authentication/authentication.service.ts
+++ src/app/core/authentication/authentication.service.ts
@@ -59,7 +59,7 @@ export class AuthenticationService {
       })
     };
      return this.httpClient
-      .post(routes.login(), context, httpOptions) 
+      .post(routes.login(), context) 
       .pipe(
         map((body: any) => {
           const data = {
