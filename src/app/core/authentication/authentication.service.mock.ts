8698612ee8be0db6e0efae8d737e378697d8b443
Randall Valenciano <ravf.226@gmail.com>
2018-04-23 09:33:21 -0600

    master: quick fix interface error in tests

--- src/app/core/authentication/authentication.service.mock.ts
+++ src/app/core/authentication/authentication.service.mock.ts
@@ -7,13 +7,17 @@ export class MockAuthenticationService {
 
   credentials: Credentials | null = {
     username: 'test',
-    token: '123'
+    token: '123',
+    id: 24,
+    email: 'test@test.com'
   };
 
   login(context: LoginContext): Observable<Credentials> {
     return of({
       username: context.username,
-      token: '123456'
+      token: '123456',
+      id: 24,
+      email: 'test@test.com'
     });
   }
 
