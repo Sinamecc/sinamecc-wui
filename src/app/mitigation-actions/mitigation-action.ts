22583468f4b3a2a27dc8387e2635b908e080eacf
Randall Valenciano <ravf.226@gmail.com>
2018-04-23 21:23:33 -0600

    sin-l28-pt2: solves type interface errors

--- src/app/mitigation-actions/mitigation-action.ts
+++ src/app/mitigation-actions/mitigation-action.ts
@@ -1,5 +1,36 @@
 export interface MitigationAction {
   name: string;
+  id: string;
+  strategy_name: string;
+  purpose: string;
+  quantitative_purpose: string;
+  institution: Institution;
+  contact: Contact;
+  location: Location;
+  progress_indicator: ProgressIndicator;
   created: string;
   updated: string;
+}
+
+export interface ProgressIndicator {
+  name: string;
+  type: string;
+  unit: string;
+  start_date: string;
+}
+export interface Contact {
+  full_name: string;
+  email: string;
+  position: string;
+  phone: string;
+}
+
+export interface Location {
+  geographical_site: string;
+  is_gis_annexed: string;
+}
+
+export interface Institution {
+  id: string;
+  name: string;
 }
