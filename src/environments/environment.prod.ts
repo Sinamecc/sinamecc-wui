a3b88153615ec744c29e41bb43552c7b97883f7c
Randall Valenciano <ravf.226@gmail.com>
2019-08-19 22:27:56 -0600

    adds real production endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ import env from '@env/.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'http://stage.sinamecc-minae.com:8000/api',
-  carbonUrl: 'http://stage.sinamecc-minae.com:8000/api',
+  serverUrl: 'http://dev-backend.sinamecc.go.cr/api',
+  carbonUrl: 'http://dev-mccr.sinamecc.go.cr/api',
   defaultLanguage: 'en-US',
   supportedLanguages: [
     'en-US',
