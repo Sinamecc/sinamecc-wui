36759acfee035e09868f1a436ddea3b827f4e04b
Bryan <bryanortiz0297@gmail.com>
2019-12-11 14:03:20 -0600

    remove extra lines

--- src/app/home/home-routing-module.ts
+++ src/app/home/home-routing-module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
-
-
-
 import { HomeComponent } from './home.component';
-
 import { Routes, RouterModule } from '@angular/router';
 import { Route, extract } from '@app/core';
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,9 +15,7 @@ import { Router } from '@angular/router';
   ]
 })
 export class HomeComponent implements OnInit {
-
-
-
+  
   dataImage= [
     {
       image:'url(assets/ma_image.jpg)',
