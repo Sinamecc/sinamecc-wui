6296c11a47b711117efed3202e76f29406d8718b
Bryan <bryanortiz0297@gmail.com>
2020-01-29 15:14:30 -0600

    add permissions to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, transition, animate, state, style, trigger } from '@angular/core';
 import { Router } from '@angular/router';
+import { Permissions } from '../core/permissions';
+import { AuthenticationService } from '../core/authentication/authentication.service';
 
 @Component({
   selector: 'app-home',
@@ -20,37 +22,54 @@ export class HomeComponent implements OnInit {
     {
       image:'url(assets/ma_image.jpg)',
       name:'MA',
-      url:'/mitigation/actions'
+      url:'/mitigation/actions',
+      moduleName:'ma'
     },
     {
       image:'url(assets/report_image.jpg)',
       name:'Report',
-      url:"/report"
+      url:"/report",
+      moduleName:''
     },
     {
       image:'url(assets/mccr_image.jpg)',
       name:'MCCR',
-      url:"/mccr/registries"
+      url:"/mccr/registries",
+      moduleName:'mccr'
     },
     {
       image:'url(assets/admin_image.jpg)',
       name:'ADMIN',
-      url:"/admin/users"
+      url:"/admin/users",
+      moduleName:'admin'
     },
     {
       image:'url(assets/ppcn_image.jpg)',
       name:'PPCN',
-      url:"/ppcn/registries"
+      url:"/ppcn/registries",
+      moduleName:'ppcn'
     },
   ]
 
-  constructor(private router: Router,) { }
+  constructor(private router: Router, private authenticationService: AuthenticationService,) { }
 
   ngOnInit() {
   }
 
   goToSite(url:string){
     this.router.navigate([url], { replaceUrl: true });
+  }
+
+  get permissions(): Permissions{
+    return this.authenticationService.credentials.permissions;
+  }
+
+  showModule(permissions:Permissions,module:string){
+    if(permissions.all){
+      return true;
+    }else{
+      return Boolean(permissions[module])
+    } 
   }
 
 }
