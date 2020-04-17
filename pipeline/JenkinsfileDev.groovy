bea0f28f48cd6cc9686c8c99bfad837353e289f0
Marco Salgado <549503+msalgado@users.noreply.github.com>
2020-04-16 18:42:17 -0600

    Adding pipeline/JenkinsfileDev.groovy

--- pipeline/JenkinsfileDev.groovy
+++ pipeline/JenkinsfileDev.groovy
@@ -1,42 +1,22 @@
 pipeline {
     agent any;
-    environment {
-        DJANGO_SETTINGS_MODULE = "config.settings.stage_aws"
-    }
 
     stages {
 
-        stage("Build and Test") {
-            steps {
-                withPythonEnv('/usr/bin/python3.6') {
-                    echo "Step: Updating requirements"
-                    sh 'pip install -r requirements.txt'
-
-                    echo "Step: Running Tests"
-                    //sh 'python manage.py test'
-
-                    echo "Step: Running Migrations"
-                    sh 'python manage.py migrate'
-                }
-            }
-        }
-
         stage ("Building docker image") {
             steps {
-                withPythonEnv('/usr/bin/python3.6') {
                     echo "Step: Building docker image"
-                    sh 'docker build -t sinamecc_backend:latest .'
-                }
+                    sh 'docker build -t sinamecc_frontend:dev .'
             }   
         }
 
         stage ("Restarting docker container") {
             steps {
                 echo "Step: Stopping current container"
-                sh 'docker stop sinamecc_backend_dev'
+                sh 'test ! -z "`docker ps | grep sinamecc_frontend_dev`" && (docker stop sinamecc_frontend_dev && docker rm sinamecc_frontend_dev) || echo "sinamecc_frontend_dev does not exists"'
 
                 echo "Step: Running new container"
-                sh 'docker run -d -e "DJANGO_SETTINGS_MODULE=config.settings.stage_aws" --name sinamecc_backend_dev sinamecc_backend:latest'
+                sh 'docker run -d -e "DJANGO_SETTINGS_MODULE=config.settings.stage_aws" --name sinamecc_frontend_dev sinamecc_frontend:dev'
             }   
         }
     }
