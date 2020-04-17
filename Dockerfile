e9ac906e6acaf90e68d165e4d9eb97c92f2c0c46
Marco Salgado <549503+msalgado@users.noreply.github.com>
2020-04-16 18:57:39 -0600

    Changing docker config, adding dev/stage env configs

--- Dockerfile
+++ Dockerfile
@@ -20,3 +20,5 @@ FROM nginx:1.13
 COPY --from=node /app/dist/ /usr/share/nginx/html
 
 COPY ./nginx-custom.conf /etc/nginx/conf.d/default.conf
+
+EXPOSE 80
--- pipeline/JenkinsfileDev.groovy
+++ pipeline/JenkinsfileDev.groovy
@@ -6,7 +6,7 @@ pipeline {
         stage ("Building docker image") {
             steps {
                     echo "Step: Building docker image"
-                    sh 'docker build -t sinamecc_frontend:dev .'
+                    sh 'docker build --build-arg env=dev -t sinamecc_frontend:dev .'
             }   
         }
 
@@ -16,7 +16,7 @@ pipeline {
                 sh 'test ! -z "`docker ps | grep sinamecc_frontend_dev`" && (docker stop sinamecc_frontend_dev && docker rm sinamecc_frontend_dev) || echo "sinamecc_frontend_dev does not exists"'
 
                 echo "Step: Running new container"
-                sh 'docker run -d -e "DJANGO_SETTINGS_MODULE=config.settings.stage_aws" --name sinamecc_frontend_dev sinamecc_frontend:dev'
+                sh 'docker run -d --name sinamecc_frontend_dev -p 8016:80 sinamecc_frontend:dev'
             }   
         }
     }
