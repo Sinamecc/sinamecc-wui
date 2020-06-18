8397f4f6e8555d17d9099d7c42e4ef002e5e1ced
Randall Valenciano <ravf.226@gmail.com>
2020-06-18 01:17:10 -0600

    jenkins: adds -a flag in order to show ALL containers, even the not running ones

--- pipeline/JenkinsfileDev.groovy
+++ pipeline/JenkinsfileDev.groovy
@@ -13,7 +13,7 @@ pipeline {
         stage ("Restarting docker container") {
             steps {
                 echo "Step: Stopping current container"
-                sh 'test ! -z "`docker ps | grep sinamecc_frontend_dev`" && (docker stop sinamecc_frontend_dev && docker rm sinamecc_frontend_dev) || echo "sinamecc_frontend_dev does not exists"'
+                sh 'test ! -z "`docker ps -a | grep sinamecc_frontend_dev`" && (docker stop sinamecc_frontend_dev && docker rm sinamecc_frontend_dev) || echo "sinamecc_frontend_dev does not exists"'
 
                 echo "Step: Running new container"
                 sh 'docker run -d --name sinamecc_frontend_dev -p 8016:80 sinamecc_frontend:dev'
