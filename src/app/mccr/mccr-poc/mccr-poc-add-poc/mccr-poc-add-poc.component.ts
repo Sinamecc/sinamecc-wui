45fb2799d3dcfc8afa3e4e0f72897a320581eb2a
Bryan <bryanortiz0297@gmail.com>
2020-06-25 13:17:34 -0600

    fix snackbar translations and prettier files

--- src/app/mccr/mccr-poc/mccr-poc-add-poc/mccr-poc-add-poc.component.ts
+++ src/app/mccr/mccr-poc/mccr-poc-add-poc/mccr-poc-add-poc.component.ts
@@ -1,69 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
-import { AbstractControl, FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
-import { MccrPocService } from '@app/mccr/mccr-poc/mccr-poc.service';
-import { finalize, tap } from 'rxjs/operators';
-import { TranslateService } from '@ngx-translate/core';
-import { MatSnackBar } from '@angular/material';
-import { Logger } from '@app/core';
+import { Router } from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
+import {
+	AbstractControl,
+	FormGroup,
+	FormBuilder,
+	Validators,
+	FormArray
+} from "@angular/forms";
+import { MccrPocService } from "@app/mccr/mccr-poc/mccr-poc.service";
+import { finalize, tap } from "rxjs/operators";
+import { TranslateService } from "@ngx-translate/core";
+import { MatSnackBar } from "@angular/material";
+import { Logger } from "@app/core";
 
-const log = new Logger('Report');
+const log = new Logger("Report");
 
 @Component({
-  selector: 'app-mccr-poc-add-poc',
-  templateUrl: './mccr-poc-add-poc.component.html',
-  styleUrls: ['./mccr-poc-add-poc.component.scss']
+	selector: "app-mccr-poc-add-poc",
+	templateUrl: "./mccr-poc-add-poc.component.html",
+	styleUrls: ["./mccr-poc-add-poc.component.scss"]
 })
 export class MccrPocAddPocComponent implements OnInit {
+	isLoading = false;
+	error: string;
+	form: FormGroup;
 
-  isLoading = false;
-  error: string;
-  form: FormGroup;
+	constructor(
+		private route: ActivatedRoute,
+		private formBuilder: FormBuilder,
+		private service: MccrPocService,
+		private router: Router,
+		private translateService: TranslateService,
+		public snackBar: MatSnackBar
+	) {
+		this.createForm();
+	}
 
-  constructor(
-    private route: ActivatedRoute,
-    private formBuilder: FormBuilder,
-    private service: MccrPocService,
-    private router: Router,
-    private translateService: TranslateService,
-    public snackBar: MatSnackBar) {
-    this.createForm();
-  }
+	ngOnInit() {}
 
-  ngOnInit() {
-  }
+	submitForm() {
+		this.isLoading = true;
+		this.service
+			.submitNewUcc(this.form.value)
+			.pipe(
+				finalize(() => {
+					this.form.markAsPristine();
+					this.isLoading = false;
+				})
+			)
+			.subscribe(
+				response => {
+					this.router.navigate(
+						[`/mccr/poc/detail/${this.form.value.uccBatchCode}`],
+						{ replaceUrl: true }
+					);
+					this.translateService
+						.get("Sucessfully submitted form")
+						.subscribe((res: string) => {
+							this.snackBar.open(res, null, { duration: 3000 });
+						});
+					log.debug(`${response.statusCode} status code received from form`);
+				},
+				error => {
+					log.debug(`Mccr Registry File error: ${error}`);
+					this.error = error;
+				}
+			);
+	}
 
-  submitForm() {
-    this.isLoading = true;
-    this.service.submitNewUcc(this.form.value)
-    .pipe(finalize(() => {
-      this.form.markAsPristine();
-      this.isLoading = false;
-    }))
-    .subscribe(response => {
-      this.router.navigate([`/mccr/poc/detail/${this.form.value.uccBatchCode}`], { replaceUrl: true });
-      this.translateService.get('Sucessfully submitted form')
-        .subscribe((res: string) => { this.snackBar.open(res, null, {duration: 3000 }); });
-      log.debug(`${response.statusCode} status code received from form`);
+	back() {
+		this.router.navigate([`/mccr/poc`], { replaceUrl: true });
+	}
 
-    }, error => {
-      log.debug(`Mccr Registry File error: ${error}`);
-      this.error = error;
-    });
-  }
-
-  back() {
-    this.router.navigate([`/mccr/poc`], { replaceUrl: true });
-  }
-
-  createForm() {
-    this.form = this.formBuilder.group({
-      uccBatchCode: ['', Validators.required],
-      uccBatchSize: ['', Validators.required],
-      userId: ['', Validators.required]
-    });
-  }
-
+	createForm() {
+		this.form = this.formBuilder.group({
+			uccBatchCode: ["", Validators.required],
+			uccBatchSize: ["", Validators.required],
+			userId: ["", Validators.required]
+		});
+	}
 }
--- src/app/shared/upload-proposal/upload-proposal.component.ts
+++ src/app/shared/upload-proposal/upload-proposal.component.ts
@@ -1,80 +1,95 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
-import { Logger, I18nService, AuthenticationService } from '@app/core';
-import { MatSnackBar } from '@angular/material';
-import { TranslateService } from '@ngx-translate/core';
-import { finalize, tap } from 'rxjs/operators';
-import { UploadProposalService } from '@app/shared/upload-proposal/upload-proposal.service';
+import {
+	Component,
+	OnInit,
+	Input,
+	OnChanges,
+	SimpleChanges,
+	Output,
+	EventEmitter
+} from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Router, ActivatedRoute } from "@angular/router";
+import { Logger, I18nService, AuthenticationService } from "@app/core";
+import { MatSnackBar } from "@angular/material";
+import { TranslateService } from "@ngx-translate/core";
+import { finalize, tap } from "rxjs/operators";
+import { UploadProposalService } from "@app/shared/upload-proposal/upload-proposal.service";
 
-
-const log = new Logger('UploadProposal');
+const log = new Logger("UploadProposal");
 
 @Component({
-  selector: 'app-upload-proposal',
-  templateUrl: './upload-proposal.component.html',
-  styleUrls: ['./upload-proposal.component.scss']
+	selector: "app-upload-proposal",
+	templateUrl: "./upload-proposal.component.html",
+	styleUrls: ["./upload-proposal.component.scss"]
 })
 export class UploadProposalComponent implements OnInit, OnChanges {
+	@Input() title: string;
+	@Input() code: string;
+	@Input() nextRoute: string;
+	@Input() formSubmitRoute: string;
+	@Input() entity: any;
+	@Input() formData: FormData;
+	@Output() formSubmitted = new EventEmitter<any>();
 
-  @Input() title: string;
-  @Input() code: string;
-  @Input() nextRoute: string;
-  @Input() formSubmitRoute: string;
-  @Input() entity: any;
-  @Input() formData: FormData;
-  @Output() formSubmitted = new EventEmitter<any>();
+	error: string;
+	form: FormGroup;
+	isLoading = false;
 
+	constructor(
+		private router: Router,
+		private i18nService: I18nService,
+		public snackBar: MatSnackBar,
+		private formBuilder: FormBuilder,
+		private translateService: TranslateService,
+		private service: UploadProposalService
+	) {}
 
+	ngOnInit() {
+		this.createForm();
+	}
 
-  error: string;
-  form: FormGroup;
-  isLoading = false;
+	ngOnChanges(changes: SimpleChanges) {
+		// this.createForm();
+	}
 
-  constructor(private router: Router,
-    private i18nService: I18nService,
-    public snackBar: MatSnackBar,
-    private formBuilder: FormBuilder,
-    private translateService: TranslateService,
-    private service: UploadProposalService) {
+	submitForm() {
+		this.isLoading = true;
+		this.formSubmitted.emit(this.form.value);
+		this.service
+			.uploadProposal(
+				this.form.value,
+				this.entity,
+				this.formSubmitRoute,
+				this.formData
+			)
+			.pipe(
+				finalize(() => {
+					this.form.markAsPristine();
+					this.isLoading = false;
+				})
+			)
+			.subscribe(
+				(response: any) => {
+					this.router.navigate([this.nextRoute], { replaceUrl: true });
+					this.translateService
+						.get("Sucessfully submitted form")
+						.subscribe((res: string) => {
+							this.snackBar.open(res, null, { duration: 3000 });
+						});
+					log.debug(`${response.statusCode} status code received from form`);
+				},
+				(error: any) => {
+					log.debug(`Upload Proposal error: ${error}`);
+					this.error = error;
+				}
+			);
+	}
 
-    }
-
-  ngOnInit() {
-    this.createForm();
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    // this.createForm();
-  }
-
-  submitForm() {
-    this.isLoading = true;
-    this.formSubmitted.emit(this.form.value);
-    this.service.uploadProposal(this.form.value, this.entity , this.formSubmitRoute, this.formData)
-      .pipe(finalize(() => {
-        this.form.markAsPristine();
-        this.isLoading = false;
-      }))
-      .subscribe((response: any) => {
-        this.router.navigate([this.nextRoute], { replaceUrl: true });
-        this.translateService
-          .get('Sucessfully submitted form')
-            .subscribe((res: string) => { this.snackBar.open(res, null, {duration: 3000 }); });
-        log.debug(`${response.statusCode} status code received from form`);
-
-      }, (error: any) => {
-        log.debug(`Upload Proposal error: ${error}`);
-        this.error = error;
-      });
-  }
-
-  private createForm() {
-    this.form = this.formBuilder.group({
-      commentCtrl: ['', Validators.required],
-      entityCtrl: [this.entity.id, Validators.required],
-      fileCtrl: [{ value: undefined, disabled: false }, []],
-    });
-  }
-
+	private createForm() {
+		this.form = this.formBuilder.group({
+			commentCtrl: ["", Validators.required],
+			entityCtrl: [this.entity.id, Validators.required],
+			fileCtrl: [{ value: undefined, disabled: false }, []]
+		});
+	}
 }
