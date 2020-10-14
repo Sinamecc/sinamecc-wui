197926f66cfbe6d39a6420852e1e2230ff99eb3d
Bryan <bryanortiz0297@gmail.com>
2020-10-14 11:32:39 -0600

    add new validations

--- src/app/ppcn/ppcn-new/ppcn-new.component.ts
+++ src/app/ppcn/ppcn-new/ppcn-new.component.ts
@@ -132,8 +132,10 @@ export class PpcnNewComponent implements OnInit, DoCheck {
 
 		const context = {
 			context: this.formGroup.value,
-			gasReportTable: this.table.buildTableSection(),
-			categoryTable: this.table.buildCategoryTableSection()
+			gasReportTable:
+				this.levelId === "2" ? this.table.buildTableSection() : null,
+			categoryTable:
+				this.levelId === "2" ? this.table.buildCategoryTableSection() : null
 		};
 
 		this.service
@@ -171,13 +173,14 @@ export class PpcnNewComponent implements OnInit, DoCheck {
 					],
 					confidentialCtrl: ["si", Validators.required],
 					confidentialValueCtrl: [""],
-					faxCtrl: "",
+					faxCtrl: this.levelId === "2" ? "" : null,
 					postalCodeCtrl: "",
 					addressCtrl: ["", Validators.required],
-					legalIdCtrl: ["", Validators.required],
+					legalIdCtrl: this.levelId === "2" ? ["", Validators.required] : null,
 					emailCtrl: this.levelId === "1" ? ["", Validators.required] : null,
 					legalRepresentativeIdCtrl: ["", Validators.required],
-					ciuuListCodeCtrl: ["", Validators.required]
+					ciuuListCodeCtrl:
+						this.levelId === "2" ? ["", Validators.required] : null
 				}),
 				this.formBuilder.group({
 					contactNameCtrl: ["", Validators.required],
@@ -209,8 +212,9 @@ export class PpcnNewComponent implements OnInit, DoCheck {
 				this.formBuilder.group({
 					baseYearCtrl: ["", Validators.required],
 					reportYearCtrl: ["", Validators.required],
-					ovvCtrl: ["", Validators.required],
-					implementationEmissionDateCtrl: ["", Validators.required],
+					ovvCtrl: this.levelId === "2" ? ["", Validators.required] : null,
+					implementationEmissionDateCtrl:
+						this.levelId === "2" ? ["", Validators.required] : null,
 					scope: ["", Validators.required]
 				}),
 				this.formBuilder.group({
@@ -289,9 +293,9 @@ export class PpcnNewComponent implements OnInit, DoCheck {
 
 	createActivityForm(): FormGroup {
 		return this.formBuilder.group({
-			activityCtrl: ["", Validators.required],
-			sectorCtrl: ["", Validators.required],
-			subSectorCtrl: ["", Validators.required]
+			activityCtrl: this.levelId === "2" ? ["", Validators.required] : null,
+			sectorCtrl: this.levelId === "2" ? ["", Validators.required] : null,
+			subSectorCtrl: this.levelId === "2" ? ["", Validators.required] : null
 		});
 	}
 
