562bc81e7e3251ca149365b5f87a4411602e533d
Bryan <bryanortiz0297@gmail.com>
2021-06-13 17:29:06 -0600

    add buildPayload method for connection with BE

--- src/app/mitigation-actions/mitigation-action-form-flow/mitigation-action-form-flow.component.ts
+++ src/app/mitigation-actions/mitigation-action-form-flow/mitigation-action-form-flow.component.ts
@@ -39,6 +39,7 @@ import { BasicInformationFormComponent } from "@app/mitigation-actions/basic-inf
 import { KeyAspectsFormComponent } from "@app/mitigation-actions/key-aspects-form/key-aspects-form.component";
 import { EmissionsMitigationFormComponent } from "@app/mitigation-actions/emissions-mitigation-form/emissions-mitigation-form.component";
 import { ImpactFormComponent } from "@app/mitigation-actions/impact-form/impact-form.component";
+import { ReportingClimateActionFormComponent } from "../reporting-climate-action-form/reporting-climate-action-form.component";
 
 @Component({
 	selector: "app-mitigation-action-form-flow",
@@ -54,7 +55,12 @@ export class MitigationActionFormFlowComponent
 	@ViewChild(EmissionsMitigationFormComponent)
 	emissionsMitigationForm: EmissionsMitigationFormComponent;
 	@ViewChild(ImpactFormComponent) impactForm: ImpactFormComponent;
-	@Input() title: string;
+
+	@ViewChild(ReportingClimateActionFormComponent)
+	reportingClimateFormComponent: ReportingClimateActionFormComponent;
+
+	@Input()
+	title: string;
 	// @Input() isLinear: boolean;
 	@Input() action: string;
 
@@ -110,7 +116,8 @@ export class MitigationActionFormFlowComponent
 				this.basicInformationFrm,
 				this.keyAspectsFrm,
 				this.emissionsMitigationFrm,
-				this.impactFrm
+				this.impactFrm,
+				this.reportingClimateFrmComponent
 			])
 		});
 	}
@@ -165,6 +172,12 @@ export class MitigationActionFormFlowComponent
 
 	get impactFrm() {
 		return this.impactForm ? this.impactForm.form : null;
+	}
+
+	get reportingClimateFrmComponent() {
+		return this.reportingClimateFormComponent
+			? this.reportingClimateFormComponent.form
+			: null;
 	}
 
 	ngAfterViewInit() {
--- src/app/mitigation-actions/reporting-climate-action-form/reporting-climate-action-form.component.ts
+++ src/app/mitigation-actions/reporting-climate-action-form/reporting-climate-action-form.component.ts
@@ -1,5 +1,5 @@
 import { DatePipe } from "@angular/common";
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, Input, OnInit, ViewChild } from "@angular/core";
 import {
 	AbstractControl,
 	FormBuilder,
@@ -10,7 +10,9 @@ import { MatSnackBar } from "@angular/material";
 import { AuthenticationService } from "@app/core";
 import { ErrorReportingComponent } from "@app/shared/error-reporting/error-reporting.component";
 import { TranslateService } from "@ngx-translate/core";
+import { Observable } from "rxjs";
 import { MitigationAction } from "../mitigation-action";
+import { MitigationActionNewFormData } from "../mitigation-action-new-form-data";
 import { MitigationActionsService } from "../mitigation-actions.service";
 
 @Component({
@@ -24,6 +26,11 @@ export class ReportingClimateActionFormComponent implements OnInit {
 	isLoading = false;
 	wasSubmittedSuccessfully = false;
 	mitigationAction: MitigationAction;
+	@Input() newFormData: Observable<MitigationActionNewFormData>;
+	@Input() processedNewFormData: MitigationActionNewFormData;
+	@Input() isUpdating: boolean;
+
+	@Input() mitigationActionToUpdate?: any;
 	@ViewChild("errorComponent") errorComponent: ErrorReportingComponent;
 
 	constructor(
@@ -34,9 +41,9 @@ export class ReportingClimateActionFormComponent implements OnInit {
 		public snackBar: MatSnackBar,
 		private datePipe: DatePipe
 	) {
-		//this.service.currentMitigationAction.subscribe(message => {
-		//	this.mitigationAction = message;
-		//});
+		this.service.currentMitigationAction.subscribe(message => {
+			this.mitigationAction = message;
+		});
 		this.createForm();
 	}
 
@@ -45,6 +52,34 @@ export class ReportingClimateActionFormComponent implements OnInit {
 	get formArray(): AbstractControl | null {
 		return this.form.get("formArray");
 	}
+
+	buildPayload() {
+		const context = {
+			monitoring_reporting_indicator: {
+				progress_in_monitoring: this.form.value.formArray[0]
+					.anyProgressMonitoringRecordedClimateActionsCtrl,
+				monitoring_indicator: [
+					{
+						initial_date_report_period: this.form.value.formArray[1]
+							.reportingPeriodStartCtrl,
+						final_date_report_period: this.form.value.formArray[1]
+							.reportingPeriodEndCtrl,
+						data_updated_date: this.form.value.formArray[1]
+							.indicatordataUpdateDateCtrl,
+						updated_data: this.form.value.formArray[1]
+							.informationWantUpdateCtrl,
+						progress_report: this.form.value.formArray[2]
+							.beenProgressActionPeriodCtrl,
+						indicator: 1
+					}
+				]
+			}
+		};
+
+		return context;
+	}
+
+	submitForm() {}
 
 	private createForm() {
 		this.form = this.formBuilder.group({
@@ -57,7 +92,10 @@ export class ReportingClimateActionFormComponent implements OnInit {
 				}),
 				this.formBuilder.group({
 					indicatorSelectionCtrl: ["", Validators.required],
-					indicatordataUpdateDateCtrl: ["", Validators.required]
+					indicatordataUpdateDateCtrl: ["", Validators.required],
+					reportingPeriodStartCtrl: ["", Validators.required],
+					reportingPeriodEndCtrl: ["", Validators.required],
+					informationWantUpdateCtrl: ["", Validators.required]
 				}),
 
 				this.formBuilder.group({
