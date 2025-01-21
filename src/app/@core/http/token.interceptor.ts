fdfd4581007f08f7f1753b0a13b9d80cc2adcefa
Bryan <bryanortiz0297@gmail.com>
2025-01-20 22:49:58 -0600

    Fix/ Remove MA Objective columns and remove purpose column, Language header set, styling components, utils file.

--- src/app/@core/http/token.interceptor.ts
+++ src/app/@core/http/token.interceptor.ts
@@ -2,17 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CredentialsService } from '@app/auth';
+import { I18nService } from '@app/i18n';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  constructor(private credentialsService: CredentialsService) {}
+  constructor(private credentialsService: CredentialsService, private i18nService: I18nService,) {}
+
+  get currentLanguage(): string {
+    return this.i18nService.language;
+  }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     if (this.credentialsService?.credentials?.token && request.url !== '/api/v1/user/admin') {
       const userToken = this.credentialsService.credentials.token;
       const headers = new HttpHeaders({
         Authorization: `${userToken}`,
-        'Accept-Language': 'es', // TODO: define leng selector
+        'Accept-Language': this.currentLanguage,
       });
       const modifiedReq = request.clone({ headers });
       return next.handle(modifiedReq);
