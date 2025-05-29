ad1d4ed5456ab3d71b28b4a906eab5ad3e23c8b7
Andrés <nosoysaenz@gmail.com>
2025-05-29 14:22:45 -0600

    chore: changed - to //

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,10 +38,10 @@ registerLocaleData(localeEsCR);
 
 export const DATE_FORMATS = {
   parse: {
-    dateInput: 'DD-MM-YYYY',
+    dateInput: 'DD/MM/YYYY',
   },
   display: {
-    dateInput: 'DD-MM-YYYY',
+    dateInput: 'DD/MM/YYYY',
     monthYearLabel: 'MMMM YYYY',
     dateA11yLabel: 'LL',
     monthYearA11yLabel: 'MMMM YYYY',
