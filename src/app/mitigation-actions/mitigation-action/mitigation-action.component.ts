af4bec7fc2c8be9d3e34742661f8d7b280de9fa2
Andrés <nosoysaenz@gmail.com>
2025-06-10 08:53:23 -0600

    fix: date format in MA view

--- src/app/mitigation-actions/mitigation-action/mitigation-action.component.ts
+++ src/app/mitigation-actions/mitigation-action/mitigation-action.component.ts
@@ -157,4 +157,14 @@ export class MitigationActionComponent implements OnInit {
 
     return commentList;
   }
+
+  dateFormat(date?: string) {
+    if (date) {
+      const parts = date.split('-');
+      if (parts.length === 3) {
+        return `${parts[2]}/${parts[1]}/${parts[0]}`;
+      }
+      return date;
+    }
+  }
 }
