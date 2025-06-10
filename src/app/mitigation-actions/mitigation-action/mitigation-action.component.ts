b417f5636dda7ef104c61dc3edd19f8c3ed05ec6
Andrés <nosoysaenz@gmail.com>
2025-06-10 16:53:52 -0600

    refactor: changed date format to pipe

--- src/app/mitigation-actions/mitigation-action/mitigation-action.component.ts
+++ src/app/mitigation-actions/mitigation-action/mitigation-action.component.ts
@@ -157,14 +157,4 @@ export class MitigationActionComponent implements OnInit {
 
     return commentList;
   }
-
-  dateFormat(date?: string) {
-    if (date) {
-      const parts = date.split('-');
-      if (parts.length === 3) {
-        return `${parts[2]}/${parts[1]}/${parts[0]}`;
-      }
-      return date;
-    }
-  }
 }
